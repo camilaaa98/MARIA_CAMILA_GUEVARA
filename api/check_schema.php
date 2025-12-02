feae38765cff008609406f1b3dd153b1baa42d8f
camilaaa98 <campguevara@gmail.com>
2025-12-02 03:34:09 -0500

    Delete api directory

--- api/check_schema.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/config/Database.php';
-$db = new Database();
-$conn = $db->getConnection();
-$tables = $conn->query("SELECT name FROM sqlite_master WHERE type='table'")->fetchAll(PDO::FETCH_COLUMN);
-print_r($tables);
-?>
